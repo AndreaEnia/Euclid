520e8132bbe9fba1df45d4700c78027b3a07bf97
Andrea Enia <enia@192.168.1.51>
2023-12-22 11:50:24 +0100

    new file:   .ipynb_checkpoints/Euclid_VIS.vis-checkpoint.dat 	new file:   .ipynb_checkpoints/PlotFiltersThroughtput-checkpoint.ipynb 	new file:   .ipynb_checkpoints/Python-checkpoint.gitignore 	new file:   .ipynb_checkpoints/README-checkpoint.md 	new file:   .ipynb_checkpoints/filters-checkpoint.pdf 	new file:   ArtAttacks_forNNPZ/.ipynb_checkpoints/MixedLabels_RefSamples-checkpoint.ipynb 	new file:   ArtAttacks_forNNPZ/.ipynb_checkpoints/RefSample_of_TrueValues-checkpoint.ipynb 	new file:   ArtAttacks_forNNPZ/MixedLabels_RefSamples.ipynb 	new file:   ArtAttacks_forNNPZ/RefSample_of_TrueValues.ipynb 	new file:   FiltersThroughput/Euclid_VIS.vis.dat 	new file:   FiltersThroughput/NISP-PHOTO-PASSBANDS-V1-H_throughput.fits 	new file:   FiltersThroughput/NISP-PHOTO-PASSBANDS-V1-J_throughput.fits 	new file:   FiltersThroughput/NISP-PHOTO-PASSBANDS-V1-Y_throughput.fits 	new file:   FiltersThroughput/PAN-STARRS_PS1.i.dat 	new file:   FiltersThroughput/PlotFiltersThroughtput.ipynb 	new file:   FiltersThroughput/R.MP9602.filt 	new file:   FiltersThroughput/Subaru_HSC.g.dat 	new file:   FiltersThroughput/Subaru_HSC.z.dat 	new file:   FiltersThroughput/U.MP9302.filt 	new file:   FiltersThroughput/filters.pdf 	new file:   NNPZ_pp/.ipynb_checkpoints/GenerateRefSample-checkpoint.py

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,6 @@
+# Euclid
+For OU-PHZ and SWG-GAE.
+
+NNPZ_pp are a couple of script to build a reference sample from Phosphoros results, and extract the quantiles from the results.
+
+ArtAttacks are direct actions on NNPZ reference samples for Enia et al., 2024 (hopefully).
---
+++ NNPZ_pp/.ipynb_checkpoints/GenerateRefSample-checkpoint.py
@@ -0,0 +1,78 @@
+import argparse
+import os, multiprocessing
+max_cores = multiprocessing.cpu_count()
+import subprocess
+from functools import partial
+from concurrent.futures import ProcessPoolExecutor
+import numpy as np, pandas as pd
+from astropy.table import Table
+from tqdm import tqdm
+import warnings
+warnings.filterwarnings("ignore")
+
+def decode_column(df, colname):
+    df[colname] = df[colname].str.decode('utf-8')
+    return
+
+def generate_pp_file(pos_file, pp, rp):
+    import os
+    pf_idx = int(pos_file.split('_')[-1].split('.')[0])
+    if os.path.exists(rp+'pp_data_{}.npy'.format(pf_idx)) == True:
+        print('Posterior idx. {} already processed'.format(pf_idx))
+        return
+    print('Processing file {}'.format(pos_file))
+    sample_posterior = Table.read(pp+pos_file).to_pandas()
+    # --------------
+    # Log M* and SFR
+    #sample_posterior['STELLARMASS'] = np.log10(sample_posterior['STELLARMASS'])
+    #sample_posterior['SFR'] = np.log10(sample_posterior['SFR'])
+    ###  already logged ###
+    # --------------
+    #decode_column(sample_posterior, 'OBJECT_ID')
+    # Seems like there's no need to decode anymore the OBJECT_ID string
+    #try: decode_column(sample_posterior, 'OBJECT_ID')
+    #except: pass
+    pp_columns = ['REDSHIFT', 'RED_CURVE_INDEX', 'EB_V', 'LUMINOSITY', 'SFHAGE', 'SFHTIMESCALE', 'SFHTYPE', 'STELLARMETALLICITY', 'STELLARMASS', 'SFR']
+    temp = [Table.from_pandas(sample_posterior[sample_posterior['OBJECT_ID'] == idx][pp_columns]) for idx in tqdm(sample_posterior['OBJECT_ID'].unique())]
+    np.save(rp+'pp_data_{}.npy'.format(pf_idx), np.array(temp))
+    print('Saved pf_idx {}'.format(pf_idx))
+    return
+
+parser = argparse.ArgumentParser(description='Generate the pp_data_{}.npy reference files for NNPZ.')
+parser.add_argument('--pos_path', type=str, help='Path to the posteriors folder.', required = True)
+parser.add_argument('--ref_path', type=str, help='Path to the reference sample folder.', required = True)
+parser.add_argument('--cores', type=int, default = 0, choices = range(1, max_cores+1), help='The maximum number of cores to use. Default is the maximum number of available cores minus 1.')
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    # Number of cores to use to run everything in parallel.
+    if args.cores == 0: max_workers = max_cores - 1
+    else: max_workers = args.cores
+
+    # First, fix the index file, adding the pp_data column mapping the right index to the right file.
+    print('Fixing index file...')
+    pp_index = Table.read(args.pos_path+'Index_File_posterior.fits').to_pandas()
+    decode_column(pp_index, 'FILE_NAME')
+    pp_index['pp_file'] = [int(s.split('_')[-1].split('.')[0]) for s in pp_index['FILE_NAME']]
+    pp_index['pp_offset'] = np.concatenate([list(np.arange(len(pp_index[pp_index['pp_file'] == idx]))) for idx in np.unique(pp_index['pp_file'])])
+    pp_index = pp_index.rename(columns = {'OBJECT_ID': 'id'})
+    pp_index = pp_index[['id', 'pp_file', 'pp_offset']]
+    pp_index['id'] = pp_index['id'].astype('int')
+    index_df = pd.DataFrame(np.load(args.ref_path+'index.npy'))
+    pp_index = pp_index.reset_index()
+    pp_index = pp_index.rename(columns={'id': 'MAMBO_id', 'index': 'id'})
+    index_df = pd.merge(index_df, pp_index, on='id', how='outer')
+    subprocess.call('mv '+args.ref_path+'index.npy '+args.ref_path+'original_index.npy', shell = True)
+    print('Moved old index.npy file to {0}/original_index.npy'.format(args.ref_path))
+    np.save(args.ref_path+'index.npy', index_df.to_records(index=False))
+    print('Index file stored in {0}/index.npy'.format(args.ref_path))
+    print('...done!')
+
+    # Read the posterior files path
+    posterior_files = [p for p in os.listdir(args.pos_path) if p.startswith('Sample')]
+
+    # Process the files in ||.
+    print('Generating the reference sample...')
+    #[generate_pp_file(pos_file, args.pos_path, args.ref_path) for pos_file in posterior_files]
+    with ProcessPoolExecutor(max_workers = max_workers) as executor: executor.map(partial(generate_pp_file, pp=args.pos_path, rp=args.ref_path), posterior_files)
+    print('Reference sample stored in {0}'.format(args.ref_path))
